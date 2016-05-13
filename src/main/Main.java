897d18be9af733a7afddcf3028a393a7281c1d09
Yuriy Yefremov <megadethov@gmail.com>
2016-05-13 23:34:16 +0300

    mod Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,6 +5,6 @@ package main;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Java");
+        System.out.printf("Hello %s! ", args[0]);
     }
 }
