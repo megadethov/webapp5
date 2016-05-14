a68bb98a5c44e1afbcb504f20f1b98b2f157eeca
Yuriy Yefremov <megadethov@gmail.com>
2016-05-14 18:57:55 +0300

    mod src/lesson02/Main.java src/ru/javawebinar/webapp/model/Link.java

--- src/lesson02/Main.java
+++ src/lesson02/Main.java
@@ -1,8 +1,13 @@
 package lesson02;
 
+import ru.javawebinar.webapp.model.Link;
+
 /**
  * Created by mega on 14.05.2016.
  */
 public class Main {
-    
+    public static void main(String[] args) {
+        Link l = new Link("javaWebinar", "javawebinar.ru");
+        System.out.println(l);
+    }
 }
--- src/ru/javawebinar/webapp/model/Link.java
+++ src/ru/javawebinar/webapp/model/Link.java
@@ -6,4 +6,9 @@ package ru.javawebinar.webapp.model;
 public class Link {
     private final String name;
     private final String url;
+
+    public Link(String name, String url) {
+        this.name = name;
+        this.url = url;
+    }
 }
