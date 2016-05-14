5d5366cd61ce555b70166b443a17bf39cc5b5e37
Yuriy Yefremov <megadethov@gmail.com>
2016-05-14 10:44:51 +0300

    new src/main/Car.java

---
+++ src/main/Car.java
@@ -0,0 +1,12 @@
+package main;
+
+/**
+ * Created by mega on 14.05.2016.
+ */
+public class Car {
+    int speed  = 100;
+
+    void go() {
+        System.out.println("Go at speed " + speed);
+    }
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -6,5 +6,8 @@ package main;
 public class Main {
     public static void main(String[] args) {
         System.out.printf("Hello %s! ", args[0]);
+        Car car = new Car();
+        car.go();
     }
+
 }
