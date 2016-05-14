bd2d1ba2410362500789f659d0075a26ef33ee1b
Yuriy Yefremov <megadethov@gmail.com>
2016-05-14 17:12:13 +0300

    mod src/ru/javawebinar/webapp/model/Resume.java

---
+++ src/ru/javawebinar/webapp/model/Link.java
@@ -0,0 +1,9 @@
+package ru.javawebinar.webapp.model;
+
+/**
+ * Created by mega on 14.05.2016.
+ */
+public class Link {
+    private String name;
+    private String url;
+}
--- src/ru/javawebinar/webapp/model/Resume.java
+++ src/ru/javawebinar/webapp/model/Resume.java
@@ -6,8 +6,7 @@ import java.util.Collection;
  * Created by mega on 13.05.2016.
  */
 public class Resume {
-    String fullName;
-    String[] myProjects;
-    Collection<String> collection;
+    private String fullName;
+    private String location;
 
 }
