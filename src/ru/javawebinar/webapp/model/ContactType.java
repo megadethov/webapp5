dc64c8ea1fe2be9f6ccd18e0eb01182f8fe5b815
Yuriy Yefremov <megadethov@gmail.com>
2016-05-15 18:48:26 +0300

    add ContactType

---
+++ src/ru/javawebinar/webapp/model/ContactType.java
@@ -0,0 +1,14 @@
+package ru.javawebinar.webapp.model;
+
+/**
+ * Created by mega on 15.05.2016.
+ */
+public enum ContactType {
+    PHONE,
+    MOBILE,
+    HOME_PHONE,
+    SKYPE,
+    MAIL,
+    ICQ
+}
+
