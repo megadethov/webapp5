a118a977bc879be1b9be470e90d1c6fb6632d47f
Yuriy Yefremov <megadethov@gmail.com>
2016-05-15 22:34:04 +0300

    enhanced ContactsType

--- src/ru/javawebinar/webapp/model/ContactType.java
+++ src/ru/javawebinar/webapp/model/ContactType.java
@@ -4,11 +4,21 @@ package ru.javawebinar.webapp.model;
  * Created by mega on 15.05.2016.
  */
 public enum ContactType {
-    PHONE,
-    MOBILE,
-    HOME_PHONE,
-    SKYPE,
-    MAIL,
-    ICQ
+    PHONE("Тел."),
+    MOBILE("Мобильный"),
+    HOME_PHONE("Домашний тел."),
+    SKYPE("Skype"),
+    MAIL("Почта"),
+    ICQ("ICQ");
+
+    private String title;
+
+    ContactType(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
 }
 
