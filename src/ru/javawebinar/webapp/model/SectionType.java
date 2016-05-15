6e1b084afc5069dc146dd5f06787ddc4e186bfc7
Yuriy Yefremov <megadethov@gmail.com>
2016-05-15 22:26:50 +0300

    enhanced SectionType

--- src/ru/javawebinar/webapp/model/SectionType.java
+++ src/ru/javawebinar/webapp/model/SectionType.java
@@ -4,9 +4,19 @@ package ru.javawebinar.webapp.model;
  * Created by mega on 15.05.2016.
  */
 public enum SectionType {
-    OBJECTIVE,
-    ACHIEVEMENT,
-    QUALIFICATIONS,
-    EXPERIENCE,
-    EDUCATION
+    OBJECTIVE("Позиция"),
+    ACHIEVEMENT("Достижения"),
+    QUALIFICATIONS("Квалификация"),
+    EXPERIENCE("Опыт работы"),
+    EDUCATION("Образование");
+
+    private String title;
+
+    SectionType(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
 }
