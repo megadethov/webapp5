9478828df038551f3dd324d5199ef09819bdda7e
Yuriy Yefremov <megadethov@gmail.com>
2016-05-16 14:04:26 +0300

    create storage

---
+++ src/ru/javawebinar/webapp/storage/ArrayStorage.java
@@ -0,0 +1,46 @@
+package ru.javawebinar.webapp.storage;
+
+import ru.javawebinar.webapp.model.Resume;
+
+import java.util.Collection;
+
+/**
+ * Created by mega on 16.05.2016.
+ */
+public class ArrayStorage implements IStorage {
+
+    @Override
+    public void clear() {
+        
+    }
+
+    @Override
+    public void save(Resume r) {
+
+    }
+
+    @Override
+    public void update(Resume r) {
+
+    }
+
+    @Override
+    public Resume load(String uuid) {
+        return null;
+    }
+
+    @Override
+    public void delete(String uuid) {
+
+    }
+
+    @Override
+    public Collection<Resume> getAllSorted() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
---
+++ src/ru/javawebinar/webapp/storage/IStorage.java
@@ -0,0 +1,19 @@
+package ru.javawebinar.webapp.storage;
+
+import ru.javawebinar.webapp.model.Resume;
+
+import java.util.Collection;
+
+/**
+ * Created by mega on 16.05.2016.
+ */
+public interface IStorage {
+    void clear(); // cleaning storage (for JUnit)
+    void save(Resume r); // CRUD - create
+    void update(Resume r); // CRUD - update
+    Resume load(String uuid); // CRUD - read (from storage)
+    void delete(String uuid); // CRUD - delete
+    Collection<Resume> getAllSorted(); // get sorted list of Resume in the storage
+    int size();
+
+}
