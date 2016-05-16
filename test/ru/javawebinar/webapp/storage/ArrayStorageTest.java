ffc91d0fcebed2f5e35ca19ad8c6422079bfd422
Yuriy Yefremov <megadethov@gmail.com>
2016-05-16 15:50:05 +0300

    Junit

---
+++ test/ru/javawebinar/webapp/storage/ArrayStorageTest.java
@@ -0,0 +1,44 @@
+package ru.javawebinar.webapp.storage;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by mega on 16.05.2016.
+ */
+public class ArrayStorageTest {
+    @org.junit.Test
+    public void clear() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void save() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void update() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void load() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void delete() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void getAllSorted() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void size() throws Exception {
+
+    }
+
+}
