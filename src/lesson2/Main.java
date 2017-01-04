828f3d5b7cc2273e462d147ba589e400135e98b6
Yuriy Yefremov <megadethov@gmail.com>
2017-01-04 22:40:36 +0200

    implement equals

---
+++ src/lesson2/Main.java
@@ -0,0 +1,14 @@
+package lesson2;
+
+import ua.javawebinar.webapp.model.Link;
+
+public class Main {
+    public static void main(String[] args) {
+        Link link = new Link("JavaWebinar", "javawebinar.ru");
+        Link link2 = link;
+        System.out.println(link.equals(link2));
+        Link link3 = new Link(link);
+        System.out.println(link.equals(link3));
+
+    }
+}
