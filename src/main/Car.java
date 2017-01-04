b9cde9f252cc3061be9aadeab636af917000f1a4
Yuriy Yefremov <megadethov@gmail.com>
2017-01-04 17:07:05 +0200

    oop Incapsulate

--- src/main/Car.java
+++ src/main/Car.java
@@ -1,9 +1,17 @@
 package main;
 
 public class Car {
-    int speed = 100;
+   private int speed = 100;
 
     void go() {
         System.out.println("Go with speed " + speed);
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
     }
 }
