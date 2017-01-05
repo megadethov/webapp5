04a2bba4371bfbf0552dbce90281d22c13c9cc6a
Yuriy Yefremov <megadethov@gmail.com>
2017-01-05 20:37:32 +0200

    add JUnit

---
+++ test/ua/javawebinar/webapp/storage/ArrayStorageTest.java
@@ -0,0 +1,43 @@
+package ua.javawebinar.webapp.storage;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ArrayStorageTest {
+    @Test
+    public void clear() throws Exception {
+
+    }
+
+    @Test
+    public void save() throws Exception {
+
+    }
+
+    @Test
+    public void update() throws Exception {
+
+    }
+
+    @Test
+    public void load() throws Exception {
+
+    }
+
+    @Test
+    public void delete() throws Exception {
+
+    }
+
+    @Test
+    public void getAllSorted() throws Exception {
+
+    }
+
+    @Test
+    public void size() throws Exception {
+
+    }
+
+}
