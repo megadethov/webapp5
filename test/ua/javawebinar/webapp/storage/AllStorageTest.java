cdd76a55143eb975b062356587df07283dacf956
Yuriy Yefremov <megadethov@gmail.com>
2017-01-11 13:16:37 +0200

    AllStorageTest

---
+++ test/ua/javawebinar/webapp/storage/AllStorageTest.java
@@ -0,0 +1,9 @@
+package ua.javawebinar.webapp.storage;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ArrayStorageTest.class, MapStorage.class, DataStreamFileStorageTest.class, SerializeFileStorageTest.class})
+public class AllStorageTest {
+}
