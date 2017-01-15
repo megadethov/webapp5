a70de6d606189e29fb00bdf403a4f45c856a9710
Yuriy Yefremov <megadethov@gmail.com>
2017-01-15 13:48:57 +0200

    First Servlet

---
+++ src/ua/javawebinar/webapp/web/ResumeServlet.java
@@ -0,0 +1,24 @@
+package ua.javawebinar.webapp.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.Writer;
+
+public class ResumeServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html; charset=UTF-8");
+        Writer w = response.getWriter();
+        w.write("Тест сервлет: привет");
+        w.close();
+    }
+}
